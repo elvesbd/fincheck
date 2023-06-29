fabcca2c45e904c9cacde1b78cfb0b2c23d2bc17
elvesbd <elvesbd@yahoo.com>
2023-06-29 13:39:25 -0300

    refactor: move decorators folder for shared

--- api/src/modules/auth/controllers/signin/signin.controller.ts
+++ api/src/modules/auth/controllers/signin/signin.controller.ts
@@ -3,7 +3,7 @@ import { SigninService } from '../../services/signin/signin.service';
 import { API_PATH } from '../constants.controller';
 import { SigninDto } from '../../dto/signin/signin.dto';
 import { SigninResponseDto } from '../../dto/signin/signin-response.dto';
-import { IsPublic } from '../../decorators/is-public.decorator';
+import { IsPublic } from 'src/shared/decorators/is-public.decorator';
 
 @Controller(API_PATH)
 export class SigninController {
