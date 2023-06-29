dc11ba024d3219fde14aa64f3ef5db06f954c1a7
elvesbd <elvesbd@yahoo.com>
2023-06-29 14:08:42 -0300

    feat: create custom decorator for extract id

--- api/src/modules/users/controllers/get-by-id/get-by-id.controller.ts
+++ api/src/modules/users/controllers/get-by-id/get-by-id.controller.ts
@@ -1,16 +1,15 @@
-import { Controller, Get, Req } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { API_PATH } from '../constants.controller';
 import { GetUserByIdService } from '../../services/get-by-id/get-by-id.service';
 import { GetByIdResponseDto } from './dto/get-by-id-response.dto';
-import { Request } from 'express';
+import { ExtractUserId } from 'src/shared/decorators/extract-user-id.decorator';
 
 @Controller(API_PATH)
 export class GetUserByIdController {
   constructor(private readonly getUserByIdService: GetUserByIdService) {}
 
   @Get('me')
-  getById(@Req() request: Request): Promise<GetByIdResponseDto> {
-    const id = request.userId;
+  getById(@ExtractUserId() id: string): Promise<GetByIdResponseDto> {
     return this.getUserByIdService.execute(id);
   }
 }
---
+++ api/src/shared/decorators/extract-user-id.decorator.ts
@@ -0,0 +1,18 @@
+import {
+  ExecutionContext,
+  UnauthorizedException,
+  createParamDecorator,
+} from '@nestjs/common';
+import { Request } from 'express';
+
+export const ExtractUserId = createParamDecorator<undefined>(
+  (data, ctx: ExecutionContext) => {
+    const request: Request = ctx.switchToHttp().getRequest();
+    const userId = request.userId;
+
+    if (!userId) {
+      throw new UnauthorizedException('');
+    }
+    return userId;
+  },
+);
