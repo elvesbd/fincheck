2b18fdf6bae58eb509ffe579fdf5157fab192cbf
elvesbd <elvesbd@yahoo.com>
2023-07-12 09:17:00 -0300

    refactor: add schema in swagger documentation

--- api/src/modules/transactions/controllers/create/dto/create-transaction.dto.ts
+++ api/src/modules/transactions/controllers/create/dto/create-transaction.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsDateString,
   IsEnum,
@@ -10,29 +11,35 @@ import {
 import { TransactionType } from 'src/modules/transactions/enum/transaction-type.enum';
 
 export class CreateTransactionDto {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   @IsUUID()
   bankAccountId: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   @IsUUID()
   categoryId: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty()
   @IsNumber()
   @IsNotEmpty()
   @IsPositive()
   value: number;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsDateString()
   date: string;
 
+  @ApiProperty({ enum: TransactionType })
   @IsNotEmpty()
   @IsEnum(TransactionType)
   type: TransactionType;
