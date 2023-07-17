51b9cd94c11cf8f064baf25b8aed839a8aa62c4a
elvesbd <elvesbd@yahoo.com>
2023-07-17 15:45:20 -0300

    test: ensure that the access token generation flow is successful

--- api/src/modules/auth/application/services/signup/signup.service.spec.ts
+++ api/src/modules/auth/application/services/signup/signup.service.spec.ts
@@ -8,6 +8,8 @@ import { Hasher } from 'src/shared/adapters/cryptography/bcrypt';
 import { Encrypt } from 'src/shared/adapters/cryptography/jwt';
 import { UserResponseDto } from 'src/modules/users/dto';
 import { UserDataBuilder } from 'src/modules/users/__mocks__/data-builder';
+import { SignupDto } from 'src/modules/auth/dto/signup';
+import { ConflictException } from '@nestjs/common';
 
 const user: UserResponseDto = UserDataBuilder.aUser().build();
 const accessToken =
@@ -44,7 +46,7 @@ describe('SignupService', () => {
   const GetUserByEmailServiceProvide = {
     provide: GetUserByEmailService,
     useValue: {
-      execute: jest.fn().mockResolvedValue(user),
+      execute: jest.fn().mockResolvedValue(null),
     },
   };
 
@@ -74,5 +76,46 @@ describe('SignupService', () => {
     expect(encrypt).toBeDefined();
     expect(createUserService).toBeDefined();
     expect(getUserByEmailService).toBeDefined();
+  });
+
+  describe('execute()', () => {
+    const signupDto: SignupDto = {
+      name: 'John Doe',
+      email: 'any_email@mail.com',
+      password: 'any_password',
+    };
+
+    it('should be ensures that getUserByEmailService.execute is called with the correct parameters', async () => {
+      await sut.execute(signupDto);
+      expect(getUserByEmailService.execute).toHaveBeenCalledTimes(1);
+      expect(getUserByEmailService.execute).toHaveBeenCalledWith(
+        signupDto.email,
+      );
+    });
+
+    it('should be return an exception if email is already in use', async () => {
+      jest.spyOn(getUserByEmailService, 'execute').mockResolvedValueOnce(user);
+      await expect(sut.execute(signupDto)).rejects.toThrow(
+        new ConflictException('This email is already in use'),
+      );
+    });
+
+    it('should be ensures that generatePasswordHash is called with the correct parameters', async () => {
+      await sut.execute(signupDto);
+      expect(hasher.hash).toHaveBeenCalledTimes(2);
+      expect(hasher.hash).toHaveBeenCalledWith(signupDto.password, 10);
+    });
+
+    it('should be ensures that generateToken is called with the correct parameters', async () => {
+      await sut.execute(signupDto);
+      expect(encrypt.signAsync).toHaveBeenCalledTimes(3);
+      expect(encrypt.signAsync).toHaveBeenCalledWith({ sub: user.id });
+    });
+
+    it('should be return an access token on success', async () => {
+      const result = await sut.execute(signupDto);
+      expect(result).toBeDefined();
+      expect(result).toStrictEqual({ accessToken });
+    });
   });
 });
