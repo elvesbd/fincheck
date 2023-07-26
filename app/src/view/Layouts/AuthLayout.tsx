d2780e8d79a6eabd4f9ac6cb5ca064d1b4b6d9ca
elvesbd <elvesbd@yahoo.com>
2023-07-26 20:58:24 -0300

    feat: add ilustration img

--- app/src/view/Layouts/AuthLayout.tsx
+++ app/src/view/Layouts/AuthLayout.tsx
@@ -1,10 +1,22 @@
-import { Outlet } from "react-router-dom";
+import illustration from '../../assets/illustration.png'
 
 export function AuthLayout() {
   return (
-    <div>
-      <h1>AuthLayout</h1>
-      <Outlet />
+    <div className="flex w-full h-full">
+      <div className="w-1/2 h-full"></div>
+      <div className="w-1/2 h-full flex justify-center items-center p-8 relative">
+        <img
+          src={illustration}
+          alt="Imagem de ilustração do dashboard"
+          className="object-cover w-full h-full max-w-[600px] max-h-[750px] select-none rounded-[32px]"
+        />
+
+        <div className="max-w-[600px] bg-white p-10 absolute rounded-b-[32px] bottom-8">
+          <p>
+            Gerencie suas finanças pessoais de uma forma simples com o fincheck, e o melhor de tudo, totalmente grátis!
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
