b6ccd71fd8f012f2845269620b95d4ced0a24dba
elvesbd <elvesbd@yahoo.com>
2023-07-27 12:14:47 -0300

    feat: create input compnent

--- app/src/view/pages/Login/index.tsx
+++ app/src/view/pages/Login/index.tsx
@@ -1,3 +1,41 @@
+import { Link } from "react-router-dom";
+import { Input } from "../../Components/Input";
+
 export function Login() {
-  return <h1>Login</h1>
+  return (
+    <div>
+      <header className="flex flex-col items-center gap-4">
+        <h1 className="text-2xl font-bold text-gray-900 tracking-[-1px]">
+          Entre em sua conta
+        </h1>
+        <p className="space-x-2">
+          <span className="text-gray-700 tracking-[-0.5px]">
+            Novo por aqui?
+          </span>
+          <Link to="/register" className="tracking-[-0.5px] font-medium text-teal-900">
+            Crie uma conta
+          </Link>
+        </p>
+      </header>
+
+      <form className="mt-[60px] flex flex-col gap-2" action="">
+        <Input
+          type="email"
+          name="email"
+          id=""
+          placeholder="E-mail"
+        />
+        <Input
+          type="password"
+          name="password"
+          id=""
+          placeholder="Senha"
+        />
+
+        <button type="submit" className="mt-2">
+          Entrar
+        </button>
+      </form>
+    </div>
+  )
 }
