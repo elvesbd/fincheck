6b33d331f3f71d81e4d6fb0f799204d9232c95c4
elvesbd <elvesbd@yahoo.com>
2023-07-27 11:12:43 -0300

    feat: applying mobile first in the layout

--- app/src/view/Layouts/AuthLayout.tsx
+++ app/src/view/Layouts/AuthLayout.tsx
@@ -1,11 +1,18 @@
+import { Outlet } from 'react-router-dom';
 import illustration from '../../assets/illustration.png';
 import { Logo } from '../Components/Logo';
 
 export function AuthLayout() {
   return (
     <div className="flex w-full h-full">
-      <div className="w-1/2 h-full"></div>
-      <div className="w-1/2 h-full flex justify-center items-center p-8 relative">
+      <div className="w-full h-full flex items-center justify-center flex-col gap-16 lg:w-1/2">
+        <Logo className="h-6 text-gray-500"/>
+        <div className="w-full max-w-[504px] px-8">
+          <Outlet />
+        </div>
+      </div>
+
+      <div className="w-1/2 h-full justify-center items-center p-8 relative hidden lg:flex">
         <img
           src={illustration}
           alt="Imagem de ilustração do dashboard"
