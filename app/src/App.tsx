bae94b0fa8c74aed79e6caee8ff94ed88b6eb338
elvesbd <elvesbd@yahoo.com>
2023-07-28 15:40:36 -0300

    feat: create auth context and hook useAuth

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Router } from "./Router";
 import { Toaster } from "react-hot-toast";
+import { AuthProvider } from "./app/contexts";
 
 
 const queryClient = new QueryClient();
@@ -8,8 +9,10 @@ const queryClient = new QueryClient();
 export function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Router />
-      <Toaster />
+      <AuthProvider>
+        <Router />
+        <Toaster />
+      </AuthProvider>
     </QueryClientProvider>
   )
 }
--- app/src/Router/AuthGuard.tsx
+++ app/src/Router/AuthGuard.tsx
@@ -1,17 +1,20 @@
 import { Navigate, Outlet } from "react-router-dom";
+import { useAuth } from "../app/hooks";
 
 interface AuthGuardProps {
   isPrivate: boolean;
 }
 
 export function AuthGuard({ isPrivate }: AuthGuardProps) {
-  const signedIn = false;
+  const { signedIn } = useAuth();
+  console.log({signedIn})
+
   if(!signedIn && isPrivate) {
     return <Navigate to='/login' replace />
   }
 
   if(signedIn && !isPrivate) {
-    return <Navigate to='/dashboard' replace />
+    return <Navigate to='/' replace />
   }
 
   return <Outlet />
