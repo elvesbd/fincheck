ec44b77cc2ed889519a526676061f48d8c65a2ce
elvesbd <elvesbd@yahoo.com>
2023-08-01 08:59:16 -0300

    feat: adding responsiveness to content

--- app/src/view/pages/Dashboard/index.tsx
+++ app/src/view/pages/Dashboard/index.tsx
@@ -1,17 +1,25 @@
 import { Logo } from "../../Components/Logo";
 import { UserMenu } from "../../Components/UserMenu";
+import { Accounts } from "./components/Accounts";
+import { Transactions } from "./components/Transactions";
 
 export function Dashboard() {
 
   return (
-   <div className="h-full w-full px-8 pb-8 pt-6 flex flex-col">
+   <div className="h-full w-full p-4 md:px-8 md:pb-8 md:pt-6 flex flex-col gap-4">
       <header className="h-12 flex items-center justify-between">
         <Logo className="h-6 text-teal-900"/>
         <UserMenu />
       </header>
 
-      <main>
-        Content
+      <main className="flex-1 flex flex-col md:flex-row gap-4">
+        <div className="w-full md:w-1/2">
+          <Accounts />
+        </div>
+
+        <div className="w-full md:w-1/2">
+          <Transactions />
+        </div>
       </main>
    </div>
   )
