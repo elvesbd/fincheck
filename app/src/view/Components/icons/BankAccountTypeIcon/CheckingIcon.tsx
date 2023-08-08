b477b70836a8d9f387bfafab063326f78e72148c
elvesbd <elvesbd@yahoo.com>
2023-08-08 18:05:26 -0300

    refactor: add correct param name

--- app/src/view/Components/icons/BankAccountTypeIcon/CheckingIcon.tsx
+++ app/src/view/Components/icons/BankAccountTypeIcon/CheckingIcon.tsx
@@ -11,7 +11,7 @@ export function CheckingIcon() {
       <path d="M15.1884 13.9492H26.8116C28.777 13.9492 30 15.3367 30 17.3001V24.6986C30 26.6621 28.777 28.0495 26.8106 28.0495H15.1884C13.223 28.0495 12 26.6621 12 24.6986V17.3001C12 15.3367 13.2289 13.9492 15.1884 13.9492Z" stroke="#495057" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
       <path d="M15.6641 20.2988V21.7019" stroke="#495057" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
       <path d="M26.3359 21.7019V20.2988" stroke="#495057" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-      <path fill-rule="evenodd" clip-rule="evenodd" d="M23.2868 21.0003C23.2868 19.7365 22.2623 18.7129 20.9994 18.7129C19.7355 18.7129 18.7109 19.7365 18.7109 21.0003C18.7109 22.2642 19.7355 23.2878 20.9994 23.2878C22.2623 23.2878 23.2868 22.2642 23.2868 21.0003Z" stroke="#495057" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+      <path fillRule="evenodd" clipRule="evenodd" d="M23.2868 21.0003C23.2868 19.7365 22.2623 18.7129 20.9994 18.7129C19.7355 18.7129 18.7109 19.7365 18.7109 21.0003C18.7109 22.2642 19.7355 23.2878 20.9994 23.2878C22.2623 23.2878 23.2868 22.2642 23.2868 21.0003Z" stroke="#495057" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
       <rect x="1" y="1" width="40" height="40" rx="20" stroke="white" strokeWidth="2"/>
     </svg>
   );
