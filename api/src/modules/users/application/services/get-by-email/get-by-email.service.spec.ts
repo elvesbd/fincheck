fc2f444cda826e41b89675408dcf6ee3d04d262f
elvesbd <elvesbd@yahoo.com>
2023-09-08 11:46:29 -0300

    test: ensures be called userRepository.getByEmail with correct value

--- api/src/modules/users/application/services/get-by-email/get-by-email.service.spec.ts
+++ api/src/modules/users/application/services/get-by-email/get-by-email.service.spec.ts
@@ -1,18 +1,44 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { GetUserByEmailService } from './get-by-email.service';
+import { UserRepository } from 'src/modules/users/repository';
+import { UserDataBuilder } from 'src/modules/users/__mocks__/user-builder';
 
 describe('GetUserByEmailService', () => {
   let sut: GetUserByEmailService;
+  let userRepository: UserRepository;
+
+  const user = UserDataBuilder.aUser().build();
 
   beforeEach(async () => {
+    jest.clearAllMocks();
+
+    const UserRepositoryProvider = {
+      provide: 'USER_REPOSITORY',
+      useValue: {
+        getByEmail: jest.fn().mockResolvedValue(user),
+      },
+    };
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [GetUserByEmailService],
+      providers: [GetUserByEmailService, UserRepositoryProvider],
     }).compile();
 
     sut = module.get<GetUserByEmailService>(GetUserByEmailService);
+    userRepository = module.get<UserRepository>('USER_REPOSITORY');
   });
 
   it('should be defined', () => {
     expect(sut).toBeDefined();
+    expect(userRepository).toBeDefined();
+  });
+
+  describe('execute()', () => {
+    const email = 'john@mail.com';
+
+    it('should be called userRepository.getByEmail with correct value', async () => {
+      await sut.execute(email);
+      expect(userRepository.getByEmail).toHaveBeenCalledTimes(1);
+      expect(userRepository.getByEmail).toHaveBeenCalledWith(email);
+    });
   });
 });
