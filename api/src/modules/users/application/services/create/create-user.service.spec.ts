25e1a58bbd863ce9958b259c7f2c34e3f1e085bc
elvesbd <elvesbd@yahoo.com>
2023-09-08 11:30:12 -0300

    test: add setup tests

--- api/src/modules/users/application/services/create/create-user.service.spec.ts
+++ api/src/modules/users/application/services/create/create-user.service.spec.ts
@@ -1,18 +1,34 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CreateUserService } from './create-user.service';
+import { UserRepository } from 'src/modules/users/repository';
+import { UserDataBuilder } from 'src/modules/users/__mocks__/user-builder';
 
 describe('CreateUserService', () => {
   let sut: CreateUserService;
+  let userRepository: UserRepository;
+
+  const user = UserDataBuilder.aUser().build();
 
   beforeEach(async () => {
+    jest.clearAllMocks();
+
+    const UserRepositoryProvider = {
+      provide: 'USER_REPOSITORY',
+      useValue: {
+        create: jest.fn().mockResolvedValue(user),
+      },
+    };
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CreateUserService],
+      providers: [CreateUserService, UserRepositoryProvider],
     }).compile();
 
     sut = module.get<CreateUserService>(CreateUserService);
+    userRepository = module.get<UserRepository>('USER_REPOSITORY');
   });
 
   it('should be defined', () => {
     expect(sut).toBeDefined();
+    expect(userRepository).toBeDefined();
   });
 });
