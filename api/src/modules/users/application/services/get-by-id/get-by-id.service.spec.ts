0cc3bf4d6d1cdb6084a2365284e6ace8274b74a4
elvesbd <elvesbd@yahoo.com>
2023-09-08 11:48:48 -0300

    test: add setup tests

--- api/src/modules/users/application/services/get-by-id/get-by-id.service.spec.ts
+++ api/src/modules/users/application/services/get-by-id/get-by-id.service.spec.ts
@@ -1,18 +1,34 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { GetUserByIdService } from './get-by-id.service';
+import { UserRepository } from 'src/modules/users/repository';
+import { UserDataBuilder } from 'src/modules/users/__mocks__/user-builder';
 
 describe('GetUserByIdService', () => {
   let sut: GetUserByIdService;
+  let userRepository: UserRepository;
+
+  const user = UserDataBuilder.aUser().build();
 
   beforeEach(async () => {
+    jest.clearAllMocks();
+
+    const UserRepositoryProvider = {
+      provide: 'USER_REPOSITORY',
+      useValue: {
+        getById: jest.fn().mockResolvedValue(user),
+      },
+    };
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [GetUserByIdService],
+      providers: [GetUserByIdService, UserRepositoryProvider],
     }).compile();
 
     sut = module.get<GetUserByIdService>(GetUserByIdService);
+    userRepository = module.get<UserRepository>('USER_REPOSITORY');
   });
 
   it('should be defined', () => {
     expect(sut).toBeDefined();
+    expect(userRepository).toBeDefined();
   });
 });
